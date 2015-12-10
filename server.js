66007c086ee3873b88a75730776fd08f9b868120
wdragon77 <cynt@nate.com>
2015-12-10 13:21:57 +0900

    server.js 수정

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-﻿var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello from Azure running node version: ' + process.version + '</br>');
-}).listen(process.env.PORT || 3000);
+﻿var voHttp = require('http');
+var voExpress = require('express');
+var voFS = require('fs');
+var voURL = require('url');
+var voPath = require('path');
+var voApp = voExpress();
+var vsRootPath = __dirname;
+
+voApp.set('port', 8124);
+voApp.use(voExpress.static(voPath.join(__dirname, 'public')));
+
+voApp.get('/ret', function (req, res) {
+    res.redirect('http://www.naver.com');
+});
+
+voHttp.createServer(voApp).listen(voApp.get('port'), function () {
+    console.log('Express server listening on port ' + voApp.get('port'));
+});
